54b43bac61326a973cf2b9f43160f4c0302f19a7
ningth <ningth_jn@163.com>
2021-11-26 00:18:52 +0800

    init

--- app_task/schema.py
+++ app_task/schema.py
@@ -1,15 +1,19 @@
 from graphene import relay
 from graphene_django.filter import DjangoFilterConnectionField
 
-from app_task.mutations import AppMutation
-from app_task.types import AppType
+from app_task.mutations import AppMutation, RobotMutation
+from app_task.types import AppType, RobotType
 
 
 class Query(object):
     app = relay.Node.Field(AppType)
     all_apps = DjangoFilterConnectionField(AppType)
 
+    robot = relay.Node.Field(RobotType)
+    all_robots = DjangoFilterConnectionField(RobotType)
+
 
 class Mutation(object):
     upsert_app = AppMutation.Field()
+    upsert_robot = RobotMutation.Field()
     pass
